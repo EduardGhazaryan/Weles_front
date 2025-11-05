ec0e6fea96149a706cae0ce7d9240a35fa2e07d3
Karen <karengevorgyan2000@gmail.com>
2025-11-05 10:18:48 +0400

    style changes

--- src/components/feautured/Feautured.jsx
+++ src/components/feautured/Feautured.jsx
@@ -38,7 +38,7 @@ export default function Feautured() {
 
   return (
     <section
-      className="  overflow-hidden bg-green-700 sm:min-h-[300px]"
+      className="  overflow-hidden bg-green-700"
       id="feautured"
     >
       {contentSliders?.length > 0 ? (
@@ -46,7 +46,7 @@ export default function Feautured() {
           // style={{
           //   backgroundImage: `url(${contentSliders[idx]?.bg_image || ""})`,
           // }}
-          className="relative md:h-[420px] sm:h-[300px] bg-no-repeat bg-cover bg-center"
+          className="relative md:h-[420px] bg-no-repeat bg-cover bg-center"
         >
 
           <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white ">
